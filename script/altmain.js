d4973b94668a4b9030c5ec74ad9fa550fad6095d
cxr2 <ciosahoulihan@gmail.com>
2022-05-29 18:35:46 +0100

    Added alt game

--- script/altmain.js
+++ script/altmain.js
@@ -1,14 +1,16 @@
 $(document).ready(function () {
-  var tMax = 4000, // animation time, ms
+  let tMax = 4000, // animation time, ms
     height = 700,
     speeds = [],
     r = [],
-    target = 123,
-    reading = 132,
+    target = Math.floor(Math.random() * 90000) + 10000,
+    reading = Math.floor(Math.random() * 90000) + 10000,
     sTarget = target.toString(),
     sReading = reading.toString(),
     numberOutput = [],
     reels = [
+      ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"],
+      ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"],
       ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"],
       ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"],
       ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"],
@@ -26,8 +28,8 @@ $(document).ready(function () {
         '</p></div><div class="reel-door">?</div>';
     });
 
-    // Add user's meter reading to fake reel for comparison
-    $(".fake-reel").each(function (i, el) {
+    // Add user's meter reading to display reel for comparison
+    $(".display-reel").each(function (i, el) {
       el.innerHTML = sReading.charAt(i);
     });
 
@@ -38,16 +40,13 @@ $(document).ready(function () {
     if (start !== undefined) return;
 
     $(".reel-door").fadeOut(100);
-    $(".lever")
-      .attr("disabled", true)
-      .addClass("button-inactive")
-      .text("Good luck!");
-    for (var i = 0, len = sTarget.length; i < len; i += 1) {
-      var intOffset =
+    $(".lever").text("Good luck!");
+    for (let i = 0, len = sTarget.length; i < len; i += 1) {
+      let intOffset =
         (parseInt(+sTarget.charAt(i) | 0) * height) / 10 - (height / 10) * 2;
       numberOutput[i] = intOffset >= 0 ? intOffset : height - height / 10;
     }
-    for (var j = 0; j < 3; ++j) {
+    for (let j = 0; j < 5; ++j) {
       speeds[j] = Math.random() + 0.7;
       r[j] = (((Math.random() * 10) | 0) * height) / 10;
     }
@@ -56,9 +55,9 @@ $(document).ready(function () {
 
   function animate(now) {
     if (!start) start = now;
-    var t = now - start || 0;
+    let t = now - start || 0;
 
-    for (var i = 0; i < 3; ++i)
+    for (let i = 0; i < 5; ++i)
       $reels[i].scrollTop =
         ((speeds[i] / tMax / 2) * (tMax - t) * (tMax - t) + numberOutput[i]) %
           height |
@@ -79,16 +78,22 @@ $(document).ready(function () {
         targetInt = sTarget.charAt(i) | 0,
         reelClass = targetReading == targetInt ? "match" : "no-match";
 
-      $(".reel:eq(" + i + "), .fake-reel:eq(" + i + ")").addClass(reelClass);
+      $(".reel:eq(" + i + "), .display-reel:eq(" + i + ")").addClass(reelClass);
       targetReading == targetInt ? matchedNumbers++ : null;
     }
 
+    const resetButton = document.getElementById("reset");
+    resetButton.addEventListener("click", () => {
+      window.location.reload();
+    });
+
     let results;
 
-    if (matchedNumbers == 3) {
-      results = "You matched all 5 numbers! you win!!!";
+    if (matchedNumbers == 5) {
+      results = "You matched all 5 numbers! Jackpot!" && coinsRemaining + 5;
     } else if (matchedNumbers == 1) {
-      results = "You matched 1 number. Better luck next time!";
+      results =
+        "You matched 1 number. Better luck next time!" && coinsRemaining + 1;
     } else {
       results =
         "You matched " + matchedNumbers + " numbers. Better luck next time!";
@@ -96,7 +101,7 @@ $(document).ready(function () {
 
     $(".results-msg").text(results);
 
-    $(".lever").text("-");
+    $(".lever").text("Spin again");
   }
 
   init();
